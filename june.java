8412dceffed6c21c54bf6cbed4a3b0b8129a9fad
athemjune <athemjune@gmail.com>
2024-03-27 13:47:38 +0300

    first commit

---
+++ june.java
@@ -0,0 +1,21 @@
+//public class junejava
+//public static void main(string[]args){
+    //system.out.printin("/how are you"/n how old are you/'');
+    //     }
+    //}
+
+
+    import java.util.Scanner;
+    public class junejava {
+        public static void main (string[]args){
+            Scanner junejava =new scanner (system.in);
+            System.out.print ("Enter your name: ");
+            String name= scanner.nextline();
+            
+
+            System.out.print("Enter your Age:");
+            int Age=scanner.nextint();
+            scanner.close()
+        
+        }
+    }
